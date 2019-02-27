e0813ec697fa1612eb6b0e0c73cdfb3a4eb47deb
danimedina97 <correodedani97@gmail.com>
2019-02-27 17:03:53 +0100

    añadido método para el cálculo de la puntuación

--- dice.py
+++ dice.py
@@ -38,8 +38,45 @@ def makeDic():
     x[d1-1]-=1
   return d
 
+def computeScore(selection):
+  selection=[0]+selection
+  score=0
+  flush6=False
+  flush=False
+  if(selection[1]>0 and selection[2]>0 and selection[3]>0 and selection[4]>0 and selection[5]>0 and selection[6]>0):score+=1500;flush=True;flush6=True;
+  else:
+    if(selection[1]>0 and selection[2]>0 and selection[3]>0 and selection[4]>0 and selection[5]>0):score+=750;flush=True
+    if(selection[2]>0 and selection[3]>0 and selection[4]>0 and selection[5]>0 and selection[6]>0):score+=750;flush=True;flush6=True
+  
+  if(selection[1]>2): score+=1000*(2**(selection[1]-3))
+  else: 
+    score+=100*selection[1]
+    if(flush): score-=100
+  
+  if(selection[2]>2): score+=200*(2**(selection[2]-3))
+  else:
+    if(selection[2]>0 and flush==False):return 0
+  
+  if(selection[3]>2): score+=300*(2**(selection[3]-3))
+  else:
+    if(selection[3]>0 and flush==False): return 0
+  
+  if(selection[4]>2): score+=400*(2**(selection[4]-3))
+  else:
+    if(selection[4]>0 and flush==False): return 0
+  
+  if(selection[5]>2): score+=500*(2**(selection[5]-3))
+  else:
+    score+=50*selection[5] 
+    if(flush): score-=50
+  
+  if(selection[6]>2): score+=600*(2**(selection[6]-3))
+  else:
+    if(selection[6]>0 and flush6==False): return 0
+
+  return score
+
 if __name__ == "__main__":
-  print(throwDice(6))
   d=makeDic()
-  print(d)
-  print(len(d[6]))
+  computeScore(throwDice(6))
+  print(computeScore([6,0,0,0,0,0]))
